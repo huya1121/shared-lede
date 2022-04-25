c4a76c143714316d739aa3e161b9acf078897475
huya1121 <huya1121@users.noreply.github.com>
2022-04-25 22:19:12 +0800

    Update 3modify.sh

--- preset-models/3modify.sh
+++ preset-models/3modify.sh
@@ -18,7 +18,7 @@ cp -f uci-scripts/* files/etc/uci-defaults
 sed -i 's/OpenWrt/RMWIFI/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #sed -i 's/wireless.default_radio${devidx}.encryption=none/wireless.default_radio${devidx}.encryption=psk-mixed/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i '/encryption/a\set wireless.default_radio${devidx}.key=coolxiaomi' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-#修改登录密码为coolxiaomi
+#修改登录密码为password
 #sed -i '/root/croot:$1$UAJmZKcZ$ZNRtgCPfHkWum/h4Ioa7N1:18676:0:99999:7:::' package/base-files/files/etc/shadow
 #切换ramips内核到5.15
 #sed -i '/KERNEL_PATCHVER/cKERNEL_PATCHVER:=5.15' target/linux/ramips/Makefile
